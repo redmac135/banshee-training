5954d2cdb1776a96a4dc9b43aafdeadc9cf80fae
redmac135 <ethanzhao1981@gmail.com>
2022-06-15 17:22:30 -0400

    add requirements and finish models

--- banshee/training/models.py
+++ banshee/training/models.py
@@ -4,11 +4,14 @@ from django.db import models
 
 
 # Managing People
+class Level(models.Model):
+    name = models.CharField(max_length=2)
+
 class Cadet(models.Model):
     firstname = models.CharField(max_length=100)
     lastname = models.CharField(max_length=100)
     rank = models.IntegerField()
-    level = models.IntegerField()
+    level = models.ForeignKey(Level, on_delete=models.SET_NULL)
 
     class Meta:
         ordering = ['level', 'rank']
@@ -26,13 +29,42 @@ class Junior(Cadet):
             rank=rank, 
             level=level
         )
-        return 
+        for po in PO.get_pos():
+            MapPORequirement.objects.create(cadet, po, 0)
+        return cadet
 
 # Managing Training POs
 class PO(models.Model):
     number = models.IntegerField()
 
+    def get_pos():
+        return PO.objects.all()
+
 class MapPORequirement(models.Model):
-    cadet = models.ForeignKey(Junior)
-    PO = models.ForeignKey(PO)
+    cadet = models.ForeignKey(Junior, on_delete=models.CASCADE)
+    PO = models.ForeignKey(PO, on_delete=models.CASCADE)
     amount = models.IntegerField
+
+    def get_cadet_qualifications(cadet):
+        return MapPORequirement.objects.filter(cadet=cadet)
+
+# Lessons
+class Lesson(models.Model):
+    title = models.CharField(max_length=256)
+    eocode = models.CharField(max_length=11)
+    po = models.ForeignKey(PO)
+    level = models.ForeignKey(Level, on_delete=models.SET_NULL)
+
+class MapCadetAttendance(models.Model):
+    cadet = models.ForeignKey(Junior, on_delete=models.CASCADE)
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+
+# Training Nights
+class TrainingPeriod(models.Model):
+    lessons = models.ManyToManyField(Lesson)
+
+class TrainingNight(models.Model):
+    date = models.DateField()
+    periodone = models.OneToOneField(TrainingPeriod, on_delete=models.SET_NULL, null=True)
+    periodtwo = models.OneToOneField(TrainingPeriod, on_delete=models.SET_NULL, null=True)
+    periodthree = models.OneToOneField(TrainingPeriod, on_delete=models.SET_NULL, null=True)
