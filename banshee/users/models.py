21dd5da573f95166b026b4efa602a9a277c87563
redmac135 <ethanzhao1981@gmail.com>
2022-08-04 16:09:45 -0400

    merge database schema refactor changes

--- banshee/users/models.py
+++ banshee/users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class TrainingSetting(models.Model):
+    duedateoffset = models.IntegerField(default=7) # days before lesson
+
+    def save(self, *args, **kwargs):
+        self.pk = 1
+        super(TrainingSetting, self).save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        pass
+
+    @classmethod
+    def load(cls):
+        obj, created = cls.objects.get_or_create(pk=1)
+        return obj
