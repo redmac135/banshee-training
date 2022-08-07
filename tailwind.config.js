1157a0011d74d5b36e8bc3ac4ce56e4a01a9dddf
redmac135 <ethanzhao1981@gmail.com>
2022-08-06 20:44:11 -0400

    upload new color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,5 +3,16 @@ module.exports = {
         "./banshee/templates/*.html",
         "./banshee/templates/**/*.html",
         "./banshee/**/templates/**/*.html",
-    ]
+    ],
+    theme: {
+        extend: {
+            colors: {
+                "clr-1": "#052324",
+                "clr-2": "#314448",
+                "clr-3": "#1DCA7F",
+                "clr-4": "#7EE8B9",
+                "clr-5": "#DBF4EA"
+            }
+        }
+    }
 };
