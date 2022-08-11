a7453e106f2623211f5ba7b6274747b0f7cd48a0
redmac135 <ethanzhao1981@gmail.com>
2022-08-10 23:34:04 -0400

    create custom green shades

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,19 @@ module.exports = {
                 "clr-2": "#314448",
                 "clr-3": "#1DCA7F",
                 "clr-4": "#7EE8B9",
-                "clr-5": "#DBF4EA"
+                "clr-5": "#DBF4EA",
+                "banshee-green": {
+                    50: "#82EDBF",
+                    100: "#70EBB6",
+                    200: "#add1eb",
+                    300: "#4CE6A3",
+                    400: "#3BE39A",
+                    500: "#29EO91",
+                    600: "#1DCA7F", // Default Clr-3
+                    700: "#1CC47B",
+                    800: "#19B370",
+                    900: "#17A165",
+                }
             }
         }
     }
