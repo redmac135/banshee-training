db1711e920f588a43d3512148f862d7462ce91f1
redmac135 <ethanzhao1981@gmail.com>
2022-08-19 12:49:30 -0400

    remove unused import

--- banshee/training/models.py
+++ banshee/training/models.py
@@ -1,6 +1,5 @@
 from datetime import datetime
 from django.db import models
-from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
 from django.contrib.contenttypes.models import ContentType
