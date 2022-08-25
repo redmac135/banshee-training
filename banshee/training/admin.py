9e93bc87ccfa9ae10e188d21e4ffd12903f0bfb4
redmac135 <ethanzhao1981@gmail.com>
2022-08-25 14:33:57 -0400

    register extra models to admin

--- banshee/training/admin.py
+++ banshee/training/admin.py
@@ -4,7 +4,9 @@ from .models import *
 # Register your models here.
 admin.site.register(Level)
 admin.site.register(Senior)
+admin.site.register(EmptyLesson)
 admin.site.register(Lesson)
+admin.site.register(Activity)
 admin.site.register(Teach)
 admin.site.register(MapSeniorTeach)
 admin.site.register(TrainingPeriod)
