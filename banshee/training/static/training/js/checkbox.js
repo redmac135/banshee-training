45e8f1b28743027e8030ababeb8f2f3c203c57cd
redmac135 <ethanzhao1981@gmail.com>
2022-09-17 12:55:18 -0400

    javascript initial slotfields on teach form

--- banshee/training/static/training/js/checkbox.js
+++ banshee/training/static/training/js/checkbox.js
@@ -1,16 +1,36 @@
+// Check already checked boxes
+window.addEventListener("load", (event) => {
+    initialcheck()
+})
+
+function initialcheck() {
+    $(".slot_checkbox_table input[type=checkbox]:checked").each(function() {
+        var label = $("label[for='" + $(this).attr('id') + "']");
+        animate_check(label);
+    })
+}
+
 function checkChild(element) {
     let child = $(element).children("input")[0];
     let label = $(element).find("label");
     child.checked = !child.checked;
     if (child.checked) {
-        label.removeClass("shadow-md");
-        label.addClass("m-3");
-        label.addClass("p-3");
-        label.addClass("bg-yellow-500");
+        animate_check(label)
     } else {
-		label.addClass("shadow-md");
-		label.removeClass("m-3");
-        label.removeClass("p-3");
-		label.removeClass("bg-yellow-500");
+		animate_uncheck(label)
 	}
+}
+
+function animate_uncheck(label) {
+    label.addClass("shadow-md");
+    label.removeClass("m-3");
+    label.removeClass("p-3");
+    label.removeClass("bg-yellow-500");
+}
+
+function animate_check(label) {
+    label.removeClass("shadow-md");
+    label.addClass("m-3");
+    label.addClass("p-3");
+    label.addClass("bg-yellow-500");
 }
